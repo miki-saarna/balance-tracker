38ffd938426c5e6afe2945641d8051a3b488b07f
Miki Saarna <86010458+miki-saarna@users.noreply.github.com>
2024-04-15 12:50:05 -0700

    Fixed migrations

--- backend/db/migrations/migrations.sql
+++ backend/db/migrations/migrations.sql
@@ -1,14 +1,14 @@
-CREATE TABLE items {
+CREATE TABLE items (
   id INT PRIMARY KEY,
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-}
+);
 
-CREATE TABLE accounts {
-  id PRIMARY KEY,
+CREATE TABLE accounts (
+  id INT PRIMARY KEY,
   item_id INT,
   account_name VARCHAR(255) NOT NULL,
   account_type VARCHAR(255) NOT NULL,
   balance DECIMAL(12, 2) NOT NULL,
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   FOREIGN KEY (item_id) REFERENCES items(id)
-}
+);
