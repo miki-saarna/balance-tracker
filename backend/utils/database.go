ffe1d93847f911962acdfba5dcad1e9632ec6db2
Miki Saarna <86010458+miki-saarna@users.noreply.github.com>
2024-04-18 19:18:31 -0700

    err handling connecting to DB

--- backend/utils/database.go
+++ backend/utils/database.go
@@ -3,16 +3,33 @@ package utils
 import (
 	"database/sql"
 	"fmt"
+	"log"
 	"os"
+
+	"github.com/joho/godotenv"
 )
 
-func ConnectDB() *sql.DB {
-	DB_USER := os.Getenv("DB_USER")
-	DB_NAME := os.Getenv("DB_NAME")
-	DB_MASTER_PASSWORD := os.Getenv("DB_MASTER_PASSWORD")
+var (
+	DB_USER            = ""
+	DB_NAME            = ""
+	DB_MASTER_PASSWORD = ""
+)
 
+func init() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Error loading .env file: %v", err)
+	}
+
+	DB_USER = os.Getenv(DB_USER)
+	DB_NAME = os.Getenv(DB_NAME)
+	DB_MASTER_PASSWORD = os.Getenv(DB_MASTER_PASSWORD)
+}
+
+func ConnectDB() *sql.DB {
 	var dbHost string = "localhost"
 	var dbPort int = 5432
+
 	var dbUser string = DB_USER
 	var dbName string = DB_NAME
 	var dbPassword string = DB_MASTER_PASSWORD
