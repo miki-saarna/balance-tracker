fb8c58e650c98ed4d3bad66f35f6242bf7724a6d
Miki Saarna <86010458+miki-saarna@users.noreply.github.com>
2024-04-18 19:29:53 -0700

    db package fix

--- backend/utils/database.go
+++ backend/utils/database.go
@@ -21,9 +21,9 @@ func init() {
 		log.Fatalf("Error loading .env file: %v", err)
 	}
 
-	DB_USER = os.Getenv(DB_USER)
-	DB_NAME = os.Getenv(DB_NAME)
-	DB_MASTER_PASSWORD = os.Getenv(DB_MASTER_PASSWORD)
+	DB_USER = os.Getenv("DB_USER")
+	DB_NAME = os.Getenv("DB_NAME")
+	DB_MASTER_PASSWORD = os.Getenv("DB_MASTER_PASSWORD")
 }
 
 func ConnectDB() *sql.DB {
@@ -33,6 +33,10 @@ func ConnectDB() *sql.DB {
 	var dbUser string = DB_USER
 	var dbName string = DB_NAME
 	var dbPassword string = DB_MASTER_PASSWORD
+
+	fmt.Println("DB_USER", DB_USER)
+	fmt.Println("DB_NAME", DB_NAME)
+	fmt.Println("DB_MASTER_PASSWORD", DB_MASTER_PASSWORD)
 
 	psqlInfo := fmt.Sprintf(
 		"host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
