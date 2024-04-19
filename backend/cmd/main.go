093a5db9cd291efeb4e565ffe0ad06c2d6c492aa
Miki Saarna <86010458+miki-saarna@users.noreply.github.com>
2024-04-18 19:18:49 -0700

    Removed unused code

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -10,32 +10,11 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
-	plaid "github.com/plaid/plaid-go/v21/plaid"
 
 	_ "github.com/lib/pq"
 )
 
-var (
-	PLAID_CLIENT_ID                      = ""
-	PLAID_SECRET                         = ""
-	PLAID_ENV                            = ""
-	PLAID_PRODUCTS                       = ""
-	PLAID_COUNTRY_CODES                  = ""
-	PLAID_REDIRECT_URI                   = ""
-	APP_PORT                             = ""
-	client              *plaid.APIClient = nil
-	// RDS_USERNAME                         = ""
-	// RDS_PASSWORD                         = ""
-	DB_USER            = ""
-	DB_NAME            = ""
-	DB_MASTER_PASSWORD = ""
-)
-
-var environments = map[string]plaid.Environment{
-	"sandbox":     plaid.Sandbox,
-	"development": plaid.Development,
-	"production":  plaid.Production,
-}
+var APP_PORT = ""
 
 func init() {
 	err := godotenv.Load()
@@ -43,23 +22,7 @@ func init() {
 		log.Fatalf("Error loading .env file: %v", err)
 	}
 
-	PLAID_CLIENT_ID := os.Getenv("PLAID_CLIENT_ID")
-	PLAID_SECRET := os.Getenv("PLAID_SECRET")
-	PLAID_ENV = os.Getenv("PLAID_ENV")
-	PLAID_PRODUCTS = os.Getenv("PLAID_PRODUCTS")
-	PLAID_COUNTRY_CODES = os.Getenv("PLAID_COUNTRY_CODES")
-	PLAID_REDIRECT_URI = os.Getenv("PLAID_REDIRECT_URI")
 	APP_PORT = os.Getenv("APP_PORT")
-
-	// RDS_USERNAME = os.Getenv("RDS_USERNAME")
-	// RDS_PASSWORD = os.Getenv("RDS_PASSWORD")
-
-	// create Plaid client
-	configuration := plaid.NewConfiguration()
-	configuration.AddDefaultHeader("PLAID-CLIENT-ID", PLAID_CLIENT_ID)
-	configuration.AddDefaultHeader("PLAID-SECRET", PLAID_SECRET)
-	configuration.UseEnvironment(environments[PLAID_ENV])
-	client = plaid.NewAPIClient(configuration)
 
 	sqlCmd.GenTables()
 }
