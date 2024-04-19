9576a84e20eeef13240728d444b2305df93d4bf4
Miki Saarna <86010458+miki-saarna@users.noreply.github.com>
2024-04-19 16:46:46 -0700

    Save balance init

--- backend/cmd/plaid/balance.go
+++ backend/cmd/plaid/balance.go
@@ -2,7 +2,10 @@ package plaidFuncs
 
 import (
 	"context"
+	"fmt"
+	"log"
 	"net/http"
+	"os"
 
 	"github.com/gin-gonic/gin"
 	utils "github.com/miki-saarna/balance-tracker/utils"
@@ -34,7 +37,34 @@ func Balance(c *gin.Context) {
 		return
 	}
 
+	db := utils.ConnectDB()
+	defer db.Close()
+
+	accounts := balancesGetResp.GetAccounts()
+
+	for _, account := range accounts {
+		// based on access_token, need to retrieve item_id
+		var item_id string
+		err := db.QueryRow("SELECT id AS item_id FROM items WHERE access_token = $1;", accessToken).Scan(&item_id)
+		if err != nil {
+			log.Fatalf("Error retreiving item_id from access_token: %v", err)
+		}
+
+		sqlBytes, err := os.ReadFile("db/sql/saveBalance.sql")
+		if err != nil {
+			log.Fatalf("Error reading SQL file: %v", nil)
+		}
+		sqlString := string(sqlBytes)
+
+		_, err = db.Exec(sqlString, account.AccountId, item_id, account.Name, account.Type, *account.Balances.Available.Get())
+		if err != nil {
+			log.Fatalf("Error saving the account: %v", err)
+		}
+	}
+
+	fmt.Println("Successfully saved accounts")
+
 	c.JSON(http.StatusOK, gin.H{
-		"accounts": balancesGetResp.GetAccounts(),
+		"accounts": accounts,
 	})
 }
---
+++ backend/db/sql/saveBalance.sql
@@ -0,0 +1,2 @@
+INSERT INTO accounts (id, item_id, account_name, account_type, balance)
+VALUES ($1, $2, $3, $4, $5);
