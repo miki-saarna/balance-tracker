69c815ef96cc0302af17b51be24d4962b15d8cbe
Miki Saarna <86010458+miki-saarna@users.noreply.github.com>
2024-04-18 19:31:19 -0700

    Added legacy code as comment

--- backend/cmd/plaid/tokens.go
+++ backend/cmd/plaid/tokens.go
@@ -157,3 +157,50 @@ func GetAccessToken(c *gin.Context) {
 		"item_id":      itemID,
 	})
 }
+
+// func ConnectRdsDb() {
+// 	// const (
+// 	// 	host     = "balance-tracker-development.c3y0c0ku8eoe.us-east-2.rds.amazonaws.com"
+// 	// 	port     = 5432
+// 	// 	user     = RDS_USERNAME
+// 	// 	password = RDS_PASSWORD
+// 	// 	dbname   = "balance-tracker-development"
+// 	// )
+
+// 	var dbName string = ""
+// 	var dbUser string = RDS_USERNAME
+// 	var dbHost string = "balance-tracker-development-v3.c3y0c0ku8eoe.us-east-2.rds.amazonaws.com"
+// 	var dbPort int = 5432
+// 	var dbEndpoint string = fmt.Sprintf("%s:%d", dbHost, dbPort)
+// 	var region string = "us-east-2"
+// 	// var region string = "us-east-1"
+
+// 	cfg, err := config.LoadDefaultConfig(context.TODO())
+// 	if err != nil {
+// 		panic("configuration error: " + err.Error())
+// 	}
+
+// 	authenticationToken, err := auth.BuildAuthToken(context.TODO(), dbEndpoint, region, dbUser, cfg.Credentials)
+
+// 	if err != nil {
+// 		panic("failed to create authentication token: " + err.Error())
+// 	}
+
+// 	dsn := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s",
+// 		dbHost, dbPort, dbUser, authenticationToken, dbName,
+// 	)
+
+// 	db, err := sql.Open("postgres", dsn)
+// 	if err != nil {
+// 		panic(err)
+// 	}
+
+// 	fmt.Println("db", db)
+
+// 	fmt.Println("init ping")
+// 	err = db.Ping()
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// 	fmt.Println("pinged!")
+// }
