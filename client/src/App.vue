c3392ebf311036a766b416c74d6f658dde5eabe1
Miki Saarna <86010458+miki-saarna@users.noreply.github.com>
2024-06-13 19:58:26 -0700

    Updated background color

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="flex flex-col">
-    <div class="px-8 bg-sky-100">
+    <div class="px-8">
       <nav className="flex space-x-8 py-4 border-b border-gray-300">
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/balances">Balances</RouterLink>
       </nav>
     </div>
-    <main className="grow px-8 py-4 bg-sky-100">
+    <main className="grow px-8 py-4">
       <RouterView />
     </main>
   </div>
