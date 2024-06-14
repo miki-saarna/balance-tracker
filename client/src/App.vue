f59703fa5462f6a3afe1277887bf50374e227b1e
Miki Saarna <86010458+miki-saarna@users.noreply.github.com>
2024-06-13 20:14:22 -0700

    Router enhancements

--- client/src/App.vue
+++ client/src/App.vue
@@ -2,8 +2,16 @@
   <div class="flex flex-col">
     <div class="px-8">
       <nav className="flex space-x-8 py-4 border-b border-gray-300">
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/balances">Balances</RouterLink>
+        <RouterLink
+          v-for="route of routes"
+          :to="route.path"
+          :class="[
+            route.path === $route.path && 'bg-blue-300',
+            'py-1 px-2 rounded select-none',
+          ]"
+        >
+          {{ route.name }}
+        </RouterLink>
       </nav>
     </div>
     <main className="grow px-8 py-4">
@@ -12,4 +20,9 @@
   </div>
 </template>
 
-<script setup></script>
+<script setup lang="ts">
+const routes = [
+  { path: "/", name: "Home" },
+  { path: "/balances", name: "Balances" },
+];
+</script>
