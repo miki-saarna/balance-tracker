b8f6e3f932633980a172a62deaad3ec887aa8b46
Miki Saarna <86010458+miki-saarna@users.noreply.github.com>
2024-06-15 15:50:35 -0700

    Init simple tooltip

--- client/src/components/AccountCard.vue
+++ client/src/components/AccountCard.vue
@@ -3,26 +3,46 @@
     class="flex justify-between py-4 border-b border-gray-300 final:border-none"
   >
     <div class="flex flex-col">
-      <div>{{ name }}</div>
-      <div>{{ type }}</div>
-      <div>${{ balance }}</div>
+      <div class="font-semibold">{{ name }}</div>
+      <div class="text-sm">{{ type }}</div>
     </div>
 
-    <div>
-      <button @click="$emit('refresh-balance')">
-        <ArrowPathIcon class="w-5" />
-      </button>
+    <div class="self-start flex items-center">
+      <div class="font-semibold">${{ balance }}</div>
+      <Tooltip>
+        <template #default="{ toggleTooltip }">
+          <EllipsisVerticalIcon
+            v-slot="{ toggleTooltip }"
+            @click="toggleTooltip"
+            class="ml-4 w-5 cursor-pointer"
+          />
+        </template>
+        <template #tooltip>
+          <div
+            class="fixed flex justify-center items-center p-2 border rounded"
+          >
+            <button @click="$emit('refresh-balance')">
+              <ArrowPathIcon class="w-5" />
+            </button>
 
-      <!-- currently not saving `persistent_account_id` within the db -->
-      <button class="ml-4" @click="$emit('remove-account')">
-        <TrashIcon class="w-5 text-red-500" />
-      </button>
+            <!-- currently not saving `persistent_account_id` within the db -->
+            <button class="ml-4" @click="$emit('remove-account')">
+              <TrashIcon class="w-5 text-red-500" />
+            </button>
+          </div>
+        </template>
+      </Tooltip>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { ArrowPathIcon, TrashIcon } from "@heroicons/vue/24/solid";
+import Tooltip from "./Tooltip.vue";
+import {
+  ArrowPathIcon,
+  TrashIcon,
+  EllipsisVerticalIcon,
+} from "@heroicons/vue/24/solid";
 
 defineProps({
   name: {
---
+++ client/src/components/Tooltip.vue
@@ -0,0 +1,12 @@
+<template>
+  <slot :toggleTooltip="() => (isVisible = !isVisible)" />
+  <Teleport to="body" v-if="isVisible">
+    <slot name="tooltip" />
+  </Teleport>
+</template>
+
+<script setup lang="ts">
+import { shallowRef } from "vue";
+
+const isVisible = shallowRef(false);
+</script>
